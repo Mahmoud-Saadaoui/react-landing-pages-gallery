8bbc9f7bf733ba0e0952b30e1c2d4726afd0e094
Mahmoud-Saadaoui <forkan.saadaoui@gmail.com>
2026-09-17 16:31:42 +0100

    fix the road bug

--- src/pages/TheRoadPage.jsx
+++ src/pages/TheRoadPage.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import "../components/TheRoad/TheRoad.css";
+import { useEffect, useState } from "react";
+import "../components/TheRoad/css/the-road.css";
 import Navbar from "../components/TheRoad/Navbar";
 import Header from "../components/TheRoad/Header";
 import Tours from "../components/TheRoad/Tours";
@@ -10,17 +10,24 @@ import Footer from "../components/TheRoad/Footer";
 const TheRoadPage = () => {
   const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+    document.documentElement.classList.add("the-road-root");
+    return () => document.documentElement.classList.remove("the-road-root");
+  }, []);
+
   return (
-    <div className={`container${open ? " change" : ""}`}>
-      <Navbar
-        onOpen={() => setOpen(true)}
-        onClose={() => setOpen(false)}
-      />
-      <Header />
-      <Tours />
-      <Stories />
-      <Contact />
-      <Footer />
+    <div className="the-road-page">
+      <div className={`container${open ? " change" : ""}`}>
+        <Navbar
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)}
+        />
+        <Header />
+        <Tours />
+        <Stories />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 };
